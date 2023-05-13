1580c05a41f42207d521dc1cd5bb62727926748d
Johan Fritz <johanfritz0234@gmail.com>
2023-05-13 15:16:35 +0200

    I have started training but am irritated that i cannot get the thing to learn properly

--- face_classification_torch.py
+++ face_classification_torch.py
@@ -3,6 +3,12 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 import numpy as np
+import matplotlib.pyplot as plt
+
+epochs=10
+lr=1e-5
+save=True
+test=True
 
 class CustomDataset(Dataset):
     def __init__(self, positive_file, negative_file):
@@ -22,17 +28,19 @@ class CustomDataset(Dataset):
             return self.negative_data[index], self.negative_labels[index]
 
 dataset=CustomDataset('LFW12.npy', 'not_faces02.npy')
-print(dataset.__getitem__(1))
-print(dataset[1])
+#print(dataset.__getitem__(1))
+#print(dataset[1][0])
 dataloader=DataLoader(dataset, batch_size=50, shuffle=True)
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(175*150, 16, dtype=torch.float32),
+            nn.Linear(175*150, 32, dtype=torch.float32),
             nn.ReLU(),
-            nn.Linear(16, 16, dtype=torch.float32),
+            nn.Linear(32, 32, dtype=torch.float32),
+            nn.ReLU(),
+            nn.Linear(32, 16, dtype=torch.float32), 
             nn.ReLU(),
             nn.Linear(16, 8, dtype=torch.float32),
             nn.ReLU(),
@@ -43,13 +51,17 @@ class NeuralNetwork(nn.Module):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
+    def knas(self, image):
+        image=torch.flatten(image)
+        logits=self.linear_relu_stack(image)
+        return logits
 device='cpu'
 model = NeuralNetwork().to(device)
-#model.load_state_dict(torch.load("model.pth"))
+model.load_state_dict(torch.load("model.pth"))
 #print(model)
-#torch.save(model.state_dict(), "model.pth")
+torch.save(model.state_dict(), "model.pth")
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -68,5 +80,23 @@ def train(dataloader, model, loss_fn, optimizer):
 
         if batch % 100 == 0:
             loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-train(dataloader, model, loss_fn, optimizer)
+            print(f"loss: {loss:>9f}  [{current:>5d}/{size:>5d}]")
+
+# for k in range(epochs):
+#     train(dataloader, model, loss_fn, optimizer)
+# if save:
+#     torch.save(model.state_dict(), "model.pth")
+image=dataset[1][0]
+guess=model.knas(image)
+print(guess)
+correct=0
+tries=4000
+print(torch.argmax(guess))
+plt.imshow(image)
+plt.show()
+for k in range(tries):
+    image=dataset[k][0]
+    guess=model.knas(image)
+    if torch.argmax(guess)==0:
+        correct+=1
+print(correct/tries)
